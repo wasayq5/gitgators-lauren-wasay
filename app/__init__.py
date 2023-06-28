f75bb6fbd92caa1c1a705e3b87d36f7470d573a2
Lauren <laurenantt@csus.edu>
2023-06-28 13:00:47 -0700

    Added dynamic menu bar and pages for education, hobbies, and work.

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/work')
+def work():
+    return render_template('work.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/education')
+def work_experience():
+    return render_template('education.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', title="MLH Fellow", url=os.getenv("URL"))
