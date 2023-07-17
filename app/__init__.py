410f28072ab0eb65d58d2cb146a30f0a4b68affd
Wasay Qureshi <wasayq5@outlook.com>
2023-07-16 21:10:40 -0500

    completed timeline posts page

--- app/__init__.py
+++ app/__init__.py
@@ -53,9 +53,17 @@ def get_time_line_post():
         ]
     }
 
-@app.route('/timeline')
+@app.route('/timeline', methods=['GET', 'POST'])
 def timeline():
-    return render_template('timeline.html', title="Timeline")
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        content = request.form['content']
+        timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    timeline_posts = TimelinePost.select().order_by(TimelinePost.created_at.desc())
+
+    return render_template('timeline.html', title="Timeline", timeline_posts=timeline_posts)
 
 
 @app.route('/work')
