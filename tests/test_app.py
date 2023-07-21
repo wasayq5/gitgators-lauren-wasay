61f8ec32a0104144534d463facc1b6c9427030c7
Wasay Qureshi <wasayq5@outlook.com>
2023-07-21 04:55:56 -0500

    tdd

--- tests/test_app.py
+++ tests/test_app.py
@@ -70,7 +70,7 @@ class AppTestCase(unittest.TestCase):
         assert "Invalid content" in html
 
         # POST request with malformed email
-        response = self.client.post("/api/timeline_post", data={"name": "John Doe" "email":"abcxyz", "content": "Hello World, I'm John!"})
+        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email":"abcxyz", "content": "Hello World, I'm John!"})
         assert response.status_code == 400
         html = response.get_data(as_text=True)
         assert "Invalid email" in html
