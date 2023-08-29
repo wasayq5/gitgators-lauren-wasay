0e51c1136904ead498a353588df3e67c2ab47f1b
Wasay Qureshi <wasayq5@outlook.com>
2023-08-28 23:17:16 -0500

    added absolute paths to navigations in redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -25,14 +25,14 @@ echo "---------------------------------------------"
 
 echo "running docker compose down"
 
-docker compose -f docker-compose.prod.yml down
+docker compose -f gitgators-lauren-wasay/docker-compose.prod.yml down
 
 echo "ran docker compose down"
 echo "---------------------------------------------"
 
 echo "running docker compose up"
 
-docker compose -f docker-compose.prod.yml up -d --build
+docker compose -f gitgators-lauren-wasay/docker-compose.prod.yml up -d --build
 
 echo "ran docker compose up"
 echo "---------------------------------------------"
